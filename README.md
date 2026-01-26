f73950db2a9e6430ea3dec095a303e397e7eb882
poseidonn2004 <casithonque12304@gmail.com>
2026-01-26 13:31:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,163 @@
+<<<<<<< HEAD
+
+# VIETCHILL - HOTEL BOOKING WEBSITE
+<br>
+
+### Table of Contents
+
+- [Abstract](#abstract)
+- [Software Requirements](#software-requirements)
+- [Instructions](#instructions)
+- [Screenshots](#screenshots)
+- [Reference](#reference)
+
+## ABSTRACT
+<p align="justify">Các trang web đặt phòng khách sạn là một bài tập thực hành hữu ích dành cho sinh viên ngành CNTT muốn học và rèn luyện kỹ năng lập trình web. Đây là ứng dụng web cho phép người dùng tìm kiếm và đặt phòng khách sạn trực tuyến, với các chức năng phổ biến như tìm kiếm theo vị trí, giá cả, tiện nghi và đặt phòng trực tiếp qua giao diện website.</p>
+
+<p align="justify">Thông qua việc xây dựng một trang web đặt phòng khách sạn, sinh viên có thể áp dụng các kiến thức về frontend, backend, và cơ sở dữ liệu. Ngoài ra, dự án này còn giúp rèn luyện kỹ năng triển khai các tính năng nâng cao như tích hợp đánh giá của khách hàng, bộ lọc tìm kiếm, và hệ thống xác nhận đặt phòng.</p>
+
+<p align="justify">Bài tập này không chỉ giúp sinh viên hiểu rõ hơn về quy trình phát triển ứng dụng web thực tế mà còn nâng cao kỹ năng giải quyết vấn đề, tổ chức hệ thống và xây dựng giao diện thân thiện với người dùng. Đây là một cơ hội tuyệt vời để chuẩn bị cho các dự án thực tế trong tương lai.</p>
+
+<br>
+
+## SOFTWARE REQUIREMENTS
+<ul type="square">
+  <li> <b> Operating System : </b> Windows, MacOS (Chưa kiểm tra trên Linux nhưng chắc ok thôi.)</li>
+   &emsp;
+  <li> <b> Frontend : </b>
+       <p align="left"> 
+          <a href="https://www.w3.org/html/" target="_blank" > 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="50" height="50"/> 
+          </a>    
+         &emsp;
+          <a href="https://www.w3schools.com/css/" target="_blank">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="50" height="50"/> 
+          </a> 
+         &emsp;
+         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> 
+           <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/>
+         </a>
+         &emsp;
+          <a href="https://getbootstrap.com" target="_blank"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> 
+          </a>
+        </p> 
+  </li>
+ <li> <b> Backend : </b>
+     <p align = "left">
+        <a href="https://www.php.net" target="_blank"> 
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="50" height="50"/> 
+       </a>
+     </p>
+   </li>
+
+  <li> <b> Database : </b>
+     <p align="left"> 
+       <a href="https://www.mysql.com/" target="_blank">
+         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="50" height="50"/> 
+       </a> 
+</p>
+   </li>
+  </ul>
+  
+  <br>
+  
+## INSTRUCTIONS
+
+1. Tải và cài đặt <a href="https://www.apachefriends.org/download.html">XAMPP</a>
+
+2. Tải tệp gzip "vietchill" từ github repo này, giải nén và copy toàn bộ thư mục
+
+4. Paste vào thư mục 'htdocs' trong thư mục gốc cài xampp. (Windows: C:\Program Files\xampp\htdocs hoặc MacOS: /Applications/XAMPP)
+
+5. Mở PHPMyAdmin trong trình duyệt `http://localhost/phpmyadmin`
+
+6. Tạo database "vietchill"
+
+7. Import `vietchill.sql` file vào database vừa tạo
+
+8. Paste URL này vào trình duyệt `http://localhost/vietchill/index.php`
+
+9. Để truy cập trang quản lý, sử dụng link tài khoản sau:
+    
+   `http://localhost/vietchill/admin/index.php`
+
+   `username:  holden`
+   
+   `password:  12345`
+   
+<br>
+
+## Hình Minh Hoạ
+
+<details>
+<summary><b> Trang chủ: </b></summary>
+<br>
+
+![index](/images/screenshots/home.png)
+
+![index](/images/screenshots/rooms.png)
+
+![index](/images/screenshots/others.png)
+
+![index](/images/screenshots/contact.png)
+
+![index](/images/screenshots/rooms-details.png)
+
+</details>
+
+<br>
+
+<details>
+<summary><b> Đăng ký: </b></summary>
+<br>
+
+![register](/images/screenshots/register.png)
+
+</details>
+
+<br>
+
+<details>
+<summary><b> Đăng nhập: </b></summary>
+<br>
+
+![login](/images/screenshots/login.png)
+
+</details>
+
+<br>
+
+<details>
+<summary><b> Hồ sơ người dùng: </b></summary>
+<br>
+
+![profile](/images/screenshots/profile.png)
+
+![booking](/images/screenshots/booking.png)
+
+</details>
+
+<br>
+
+<details>
+<summary><b> Admin Dashboard: </b></summary>
+<br>
+
+![admin](/images/screenshots/admin.png)
+
+![admin](/images/screenshots/admin-dashboard.png)
+
+![admin](/images/screenshots/admin-rooms.png)
+
+![admin](/images/screenshots/new-bookings.png)
+
+![admin](/images/screenshots/page-setting.png)
+
+</details>
+
+
+
+=======
+# DoAnCoSo
+>>>>>>> 1b5bae177eea409bb0f541dd8ec4106971d6f680
---
+++ admin/inc/essentials.php
@@ -0,0 +1,195 @@
+<?php
+
+  //frontend purpose data
+  define('SITE_URL', 'http://localhost/vietchill/');
+  define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
+  define('CAROUSEL_IMG_PATH',SITE_URL.'images/carousel/');
+  define('FACILITIES_IMG_PATH',SITE_URL.'images/facilities/');
+  define('ROOMS_IMG_PATH',SITE_URL.'images/rooms/');
+  define('USERS_IMG_PATH',SITE_URL.'images/users/');
+  define('SERVICES_IMG_PATH',SITE_URL.'images/services/');
+
+  //backend upload process needs this data
+  define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/vietchill/images/');
+  define('ABOUT_FOLDER','about/');
+  define('CAROUSEL_FOLDER','carousel/');
+  define('FACILITIES_FOLDER','facilities/');
+  define('ROOMS_FOLDER','rooms/');
+  define('USERS_FOLDER','users/');
+  define('SERVICES_FOLDER','services/');
+
+	function adminLogin() {
+		session_start();
+		if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)){
+			echo"<script>window.location.href='index.php'</script>";
+			exit;
+		}
+	}
+
+	function redirect($url) {
+		echo "<script>window.location.href='$url'</script>";
+		exit;
+	}
+
+	function alert($type, $msg) {
+		$bs_class = ($type == 'success') ? 'alert-success' : 'alert-danger';
+		echo <<<alert
+			<div class="alert $bs_class alert-dismissible fade show custom-alert" role="alert">
+				<strong class="me-3">$msg</strong>
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+		alert;
+	}
+
+  function uploadImage($image, $folder) {
+    $valid_mime = ['image/jpeg', 'image/png', 'image/webp'];
+    $img_mime = $image['type'];
+
+    if (!in_array($img_mime, $valid_mime)) {
+      return 'Không hỗ trợ định dạng này!';
+    } else if (($image['size'] / (1024 * 1024)) > 2) {
+      return 'Vui lòng chọn hình ảnh dưới 2MB!';
+    } else {
+      $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+      $rname = 'IMG_'.random_int(11111,99999).".$ext";
+      
+      $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
+      
+      // Create directory if it doesn't exist
+      $dir = UPLOAD_IMAGE_PATH . $folder;
+      if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+      }
+      
+      if (move_uploaded_file($image['tmp_name'], $img_path)) {
+        return $rname;
+      } else {
+        return 'Tải lên hình ảnh thất bại!';
+      }
+    }
+  }
+
+  function deleteImage($image, $folder) {
+    if($image == 'default.jpg') {
+      return true; // Don't delete default image
+    }
+    
+    $img_path = UPLOAD_IMAGE_PATH.$folder.$image;
+    if(file_exists($img_path) && unlink($img_path)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function uploadSVGImage($image,$folder) {
+    $valid_mime = ['image/svg+xml'];
+    $img_mime = $image['type'];
+
+    if(!in_array($img_mime,$valid_mime)){
+      return 'Không hỗ trợ định dạng này!';
+    }
+    else if(($image['size']/(1024*1024))>1){
+      return 'Vui lòng chọn hình ảnh dưới 1MB!';
+    }
+    else{
+      $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+      $rname = 'IMG_'.random_int(11111,99999).".$ext";
+
+      $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+      
+      // Create directory if it doesn't exist
+      $dir = UPLOAD_IMAGE_PATH.$folder;
+      if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+      }
+      
+      if(move_uploaded_file($image['tmp_name'],$img_path)){
+        return $rname;
+      }
+      else{
+        return 'Tải lên hình ảnh thất bại!';
+      }
+    }
+  }
+
+	function uploadUserImage($image) {
+    $valid_mime = ['image/jpeg','image/png','image/webp'];
+    $img_mime = $image['type'];
+
+    if(!in_array($img_mime,$valid_mime)){
+      return 'inv_img'; // invalid image mime or format
+    }
+    else if(($image['size']/(1024*1024)) > 2) {
+      return 'inv_size'; // image too large
+    }
+    else {
+      $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+      $rname = 'IMG_'.random_int(11111,99999).".jpeg";
+
+      $img_path = UPLOAD_IMAGE_PATH.USERS_FOLDER.$rname;
+
+      // Create users directory if it doesn't exist
+      $users_dir = UPLOAD_IMAGE_PATH.USERS_FOLDER;
+      if (!is_dir($users_dir)) {
+        if (!mkdir($users_dir, 0777, true)) {
+          return 'upd_failed';
+        }
+      }
+
+      // Create image resource based on file type
+      $img = false;
+      if($ext == 'png' || $ext == 'PNG') {
+        $img = imagecreatefrompng($image['tmp_name']);
+      }
+      else if($ext == 'webp' || $ext == 'WEBP') {
+        $img = imagecreatefromwebp($image['tmp_name']);
+      }
+      else if($ext == 'jpg' || $ext == 'jpeg' || $ext == 'JPG' || $ext == 'JPEG') {
+        $img = imagecreatefromjpeg($image['tmp_name']);
+      }
+
+      if($img === false) {
+        return 'upd_failed';
+      }
+
+      // Convert to JPEG and save
+      if(imagejpeg($img, $img_path, 75)){
+        imagedestroy($img); // Free memory
+        return $rname;
+      }
+      else{
+        if($img) imagedestroy($img);
+        return 'upd_failed';
+      }
+    }
+  }
+
+  // Function to create default user image if it doesn't exist
+  function createDefaultUserImage() {
+    $default_path = UPLOAD_IMAGE_PATH.USERS_FOLDER.'default.jpg';
+    
+    if (!file_exists($default_path)) {
+      // Create users directory if it doesn't exist
+      $users_dir = UPLOAD_IMAGE_PATH.USERS_FOLDER;
+      if (!is_dir($users_dir)) {
+        mkdir($users_dir, 0777, true);
+      }
+      
+      // Create a simple default image (100x100 gray square)
+      $img = imagecreate(100, 100);
+      $bg = imagecolorallocate($img, 200, 200, 200);
+      $text_color = imagecolorallocate($img, 100, 100, 100);
+      
+      // Add text
+      imagestring($img, 3, 25, 40, 'USER', $text_color);
+      
+      // Save as JPEG
+      imagejpeg($img, $default_path, 75);
+      imagedestroy($img);
+    }
+  }
+
+  // Call this function to ensure default image exists
+  createDefaultUserImage();
+?>
